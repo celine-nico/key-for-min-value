288e9545a6807c79091faa1104dba47c2a3ea334
Cel <celinenico22@gmail.com>
2021-02-26 21:51:09 -0500

    commit tree ab000647eb161a70a2a04efee5b63f5de2253198

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+    key_lowest = nil
+    value_lowest = nil
+    name_hash.each do |key, value| 
+        if value_lowest == nil || value < value_lowest
+            value_lowest = value
+            key_lowest = key
+        end 
+    end
+    key_lowest 
 end
